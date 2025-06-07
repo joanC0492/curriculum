3f6338f29bc5f5ed8406eb58cd8fa0b60ed44f34
Joan C <joan0492@gmail.com>
2025-06-06 19:41:07 -0500

    first commit

---
+++ src/data/cvData.js
@@ -0,0 +1,162 @@
+export const cvData = {
+  personalInfo: {
+    image: {
+      src: "profile_cv.webp",
+      alt: "Foto de perfil de Joan Omar Cochachi Chiuayari",
+    },
+    name: "JOAN OMAR COCHACHI CHIUYARI",
+    lastName: "COCHACHI CHIUYARI",
+    email: "joan0492@gmail.com",
+    phone: "900556665",
+    linkedin: "in/joancochachi04/",
+    location: "San Juan de Lurigancho - Lima",
+    presentation: `<p>¡Hola! Soy Joan, disfruto desarrollar sitios web implementando tecnologías eficientes y modernas, trabajando como frontend developer en lenguajes como html, css y javascript con un marco de trabajo (Scrum).</p>
+      <p class="mt-3">Busco ampliar mis conocimientos en base a nuevos retos y proyectos.</p>`,
+  },
+  skills: [
+    {
+      name: "HTML",
+      level: 100,
+    },
+    {
+      name: "CSS",
+      level: 100,
+    },
+    {
+      name: "SASS",
+      level: 100,
+    },
+    {
+      name: "JAVASCRIPT",
+      level: 100,
+    },
+    {
+      name: "REACT",
+      level: 75,
+    },
+    {
+      name: "NEXTJS",
+      level: 65,
+    },
+    {
+      name: "NODEJS",
+      level: 55,
+    },
+    {
+      name: "MYSQL",
+      level: 65,
+    },
+    {
+      name: "MONGODB",
+      level: 55,
+    },
+    {
+      name: "GIT",
+      level: 55,
+    },
+    {
+      name: "TAILWIND",
+      level: 75,
+    },
+    // {
+    //   name: "BOOTSTRAP",
+    //   level: 80,
+    // },
+    // {
+    //   name: "JQUERY",
+    //   level: 75,
+    // },
+    {
+      name: "WORDPRESS",
+      level: 100,
+    },
+  ],
+  languages: [
+    {
+      name: "INGLÉS",
+      level: 45,
+    },
+    {
+      name: "ESPAÑOL",
+      level: 100,
+    },
+  ],
+  experience: [
+    {
+      company: "MEDIA IMPACT",
+      position: "Frontend developer",
+      period: "Febrero 2018 – 2020",
+      responsibilities: [
+        "Maquetación de sitios web con web responsive",
+        "Mantenimiento de Base de Datos",
+      ],
+    },
+    {
+      company: "IDEAS LATAM SAC",
+      position: "Frontend developer",
+      period: "Setiembre 2020 – Enero 2021",
+      responsibilities: [
+        "Creación de sistemas web usando Wordpress.",
+        "Implementación de Google Tag Manager y Analytic en Webs y Landing Pages.",
+      ],
+    },
+    {
+      company: "SRBURNS PERÚ",
+      position: "Fullstack Developer",
+      period: "Enero 2021 – Julio 2021",
+      responsibilities: [
+        "Mejoras en la web de Entel Empresas, tanto Frontend como Backend (https://www.entel.pe/empresas/).",
+        "Mantenimiento y creación de funcionalidades",
+      ],
+    },
+    {
+      company: "HAVAS+ SAC",
+      position: "CRO UX Analyst & Fullstack Developer",
+      period: "Agosto 2021 – Abril 2022",
+      responsibilities: [
+        "Implementar experimentos que ayudaron a incrementar la conversión de leads, para Movistar.",
+        "Desarrollo de nuevas funcionalidades en el catálogo de Movistar (https://catalogo.movistar.com.pe/ ).",
+        "Creación de experimentos (A / B y otros), utilizando Google Optimize",
+      ],
+    },
+    {
+      company: "AMAZON ARGUZ S.A.C.",
+      position: "Desarrollador WordPress",
+      period: "Julio 2024 – Marzo 2025",
+      responsibilities: [
+        "Desarrollo de sitios WordPress personalizados con temas hijos, Elementor y DIVI, optimizados para móviles.",
+        "Integración de pagos con Culqi y Mercado Pago, y personalización de tiendas con WooCommerce.",
+        "Programación de funciones a medida con PHP, shortcodes y hooks nativos.",
+        "Optimización de velocidad y SEO técnico (caché, lazy load, estructura de encabezados).",
+        "Implementación de sistemas educativos y de gamificación con LearnPress y GamiPress.",
+      ],
+    },
+    {
+      company: "FREELANCE",
+      position: "Frontend Developer",
+      period: "Mayo 2022 – Actualmente",
+      responsibilities: [
+        "Desarrollo de landing pages en HubSpot CMS.",
+        "Sitio web para cirujano con WordPress y Elementor.",
+        "Personalización de WP-Rentals en proyecto wordpress, implementando: WooCommerce, Mercado Pago y mapas.",
+        "Mailings responsivos para BCP (Outlook Desktop compatible).",
+        "Desarrollo y mantenimiento de proyectos en ReactJS.",
+      ],
+    },
+  ],
+  education: {
+    degree: "INGENIERÍA DE SISTEMAS",
+    institution: "UNIVERSIDAD NACIONAL FEDERICO VILLARREAL",
+  },
+  projects: [
+    "https://www.colectivo23.com/the-unconference-23",
+    "https://dreduardoflorescirugia.pe/",
+    "https://spaciuz.com/",
+    "http://www.piscovinasdeoro.com.pe",
+    "https://www.infopesa.com",
+    "https://mediaimpact.pe/",
+    "https://www.entel.pe/empresas/",
+    "https://www.movistar.com.pe/",
+    "https://catalogo.movistar.com.pe/",
+  ],
+};
